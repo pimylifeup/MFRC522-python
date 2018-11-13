b51ffadf2333e89d6f238eabe96b2f3e452642c5
marvelousoo <44493233+marvelousoo@users.noreply.github.com>
2018-11-13 20:36:38 +0800

    Update SimpleMFRC522.py
    1，Fix the "read_id_no_block", return should be one "None"
    2,  Define authblockaddrs and datablockaddrs to support read all blocks of RFID card.
    3,  Support unicode like chinese write into RFID card.

--- SimpleMFRC522.py
+++ SimpleMFRC522.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 # Code by Simon Monk https://github.com/simonmonk/
+
 
 import MFRC522
 import RPi.GPIO as GPIO
@@ -8,33 +11,33 @@ class SimpleMFRC522:
   READER = None;
   
   KEY = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
-  BLOCK_ADDRS = [8, 9, 10]
+  #BLOCK_ADDRS = [8,9,10]
   
   def __init__(self):
     self.READER = MFRC522.MFRC522()
   
-  def read(self):
-      id, text = self.read_no_block()        
+  def read(self, authblockaddrs, datablockaddrs):
+      id, text = self.read_no_block(authblockaddrs, datablockaddrs)
       while not id:
-          id, text = self.read_no_block()  
+          id, text = self.read_no_block(authblockaddrs, datablockaddrs)
       return id, text
 
   def read_id(self):
-    id = self.read_id_no_block()
-    while not id:
       id = self.read_id_no_block()
-    return id
+      while not id:
+          id = self.read_id_no_block()
+      return id
 
   def read_id_no_block(self):
       (status, TagType) = self.READER.MFRC522_Request(self.READER.PICC_REQIDL)
       if status != self.READER.MI_OK:
-          return None, None
+          return None
       (status, uid) = self.READER.MFRC522_Anticoll()
       if status != self.READER.MI_OK:
-          return None, None
+          return None
       return self.uid_to_num(uid)
   
-  def read_no_block(self):
+  def read_no_block(self, authblockaddrs, datablockaddrs):
     (status, TagType) = self.READER.MFRC522_Request(self.READER.PICC_REQIDL)
     if status != self.READER.MI_OK:
         return None, None
@@ -43,11 +46,11 @@ class SimpleMFRC522:
         return None, None
     id = self.uid_to_num(uid)
     self.READER.MFRC522_SelectTag(uid)
-    status = self.READER.MFRC522_Auth(self.READER.PICC_AUTHENT1A, 11, self.KEY, uid)
+    status = self.READER.MFRC522_Auth(self.READER.PICC_AUTHENT1A, authblockaddrs, self.KEY, uid)
     data = []
     text_read = ''
     if status == self.READER.MI_OK:
-        for block_num in self.BLOCK_ADDRS:
+        for block_num in datablockaddrs:
             block = self.READER.MFRC522_Read(block_num) 
             if block:
             		data += block
@@ -58,14 +61,14 @@ class SimpleMFRC522:
     
 
     
-  def write(self, text):
-      id, text_in = self.write_no_block(text)        
+  def write(self, text, authblockaddrs, datablockaddrs):
+      id, text_in = self.write_no_block(text, authblockaddrs, datablockaddrs)
       while not id:
-          id, text_in = self.write_no_block(text)  
+          id, text_in = self.write_no_block(text, authblockaddrs, datablockaddrs)
       return id, text_in
 
 
-  def write_no_block(self, text):
+  def write_no_block(self, text, authblockaddrs, datablockaddrs):
       (status, TagType) = self.READER.MFRC522_Request(self.READER.PICC_REQIDL)
       if status != self.READER.MI_OK:
           return None, None
@@ -74,17 +77,17 @@ class SimpleMFRC522:
           return None, None
       id = self.uid_to_num(uid)
       self.READER.MFRC522_SelectTag(uid)
-      status = self.READER.MFRC522_Auth(self.READER.PICC_AUTHENT1A, 11, self.KEY, uid)
-      self.READER.MFRC522_Read(11)
+      status = self.READER.MFRC522_Auth(self.READER.PICC_AUTHENT1A, authblockaddrs, self.KEY, uid)
+      self.READER.MFRC522_Read(authblockaddrs)
       if status == self.READER.MI_OK:
           data = bytearray()
-          data.extend(bytearray(text.ljust(len(self.BLOCK_ADDRS) * 16).encode('ascii')))
+          data.extend(bytearray(text.ljust(len(datablockaddrs) * 16).encode('utf-8')))
           i = 0
-          for block_num in self.BLOCK_ADDRS:
+          for block_num in datablockaddrs:
             self.READER.MFRC522_Write(block_num, data[(i*16):(i+1)*16])
             i += 1
       self.READER.MFRC522_StopCrypto1()
-      return id, text[0:(len(self.BLOCK_ADDRS) * 16)]
+      return id, text[0:(len(datablockaddrs) * 16)]
       
   def uid_to_num(self, uid):
       n = 0
